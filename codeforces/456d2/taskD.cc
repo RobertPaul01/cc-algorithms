d563dc4442ddf7998dd1f16d371a7325902f1237
Robert <robert20paul@gmail.com>
2018-02-19 01:12:45 -0500

    TaskD

--- codeforces/456d2/taskD.cc
+++ codeforces/456d2/taskD.cc
@@ -6,11 +6,51 @@ using namespace std;
 #define fin(ds) for (auto &v : ds) cin >> v;
 #define all(a) (a).begin(), (a).end()
 #define pii pair<int, int>
+#define pll pair<ll, ll>
 #define nl '\n'
 
 typedef long long ll;
 
+ll n, m, r, k;
+
 signed main() { _
-    int n, m, r, k;
     cin >> n >> m >> r >> k;
+    auto f = [](pll p) {
+        return (min(n + 1, p.first + r) - max(p.first, r)) * (min(m + 1, p.second + r) - max(p.second, r));
+    };
+    auto cmp = [&](pll s, pll t) {
+        return f(s) < f(t);
+    };
+    priority_queue<pll, vector<pll>, decltype(cmp)> pq(cmp);
+    set<pll> s;
+    
+    ll sum = 0;
+    pll start = {(n+1)/2, (m+1)/2};
+    pq.push(start);
+    while (!pq.empty() && k > 0) {
+        pll t = pq.top();
+        pq.pop();
+        if (s.count(t)) {
+            continue;
+        }
+        s.insert(t);
+        sum += f(t);
+        k--;
+        if (!s.count({t.first+1, t.second}))
+            pq.push({t.first+1, t.second});
+        
+        if (!s.count({t.first-1, t.second}))
+            pq.push({t.first-1, t.second});
+        
+        if (!s.count({t.first, t.second+1}))
+            pq.push({t.first, t.second+1});
+        
+        if (!s.count({t.first, t.second-1}))
+            pq.push({t.first, t.second-1});
+    }
+
+    double area = (n-r+1) * (m-r+1);
+    double prob = sum * (1.0f/area);
+    
+    printf("%.10f\n", prob);
 }
