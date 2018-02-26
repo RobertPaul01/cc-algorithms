435ee4b7fa479f3547375d23e6a8734c2b386f7b
Robert <robert20paul@gmail.com>
2018-02-25 23:43:21 -0600

    regular update

---
+++ codeforces/462d2/taskC.cc
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define forn(i, n) for (int i = 0; i < n; i++)
+#define fin(ds) for (auto &v : ds) cin >> v;
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define nl '\n'
+
+typedef long long ll;
+
+const int N = 2005;
+const int M = 4;
+int arr[N];
+int dp[M] = { 0 };
+
+signed main() { _
+    int n; cin >> n;
+    forn(i, n) cin >> arr[i];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == 1){
+            dp[0]++;
+            dp[2] = max(dp[1] + 1, dp[2] + 1);
+        } else if (arr[i] == 2) {
+            dp[1] = max(dp[0] + 1, dp[1] + 1);
+            dp[3] = max(dp[2] + 1, dp[3] + 1);
+        }
+    }
+
+    cout << max(dp[2], dp[3]) << endl;
+}
---
+++ codeforces/462d2/taskD.cc
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define forn(i, n) for (int i = 0; i < n; i++)
+#define fin(ds) for (auto &v : ds) cin >> v;
+#define all(a) (a).begin(), (a).end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define nl '\n'
+
+typedef long long ll;
+
+ll p;
+int k;
+int cnt = 0;
+int nums[105];
+
+signed main() { _
+    cin >> p >> k;
+    while (p != 0) {
+        int x = p % k;
+        if (x < 0) x += k;
+        x %= k;
+        nums[cnt] = x;
+        cnt++;
+        p -= x; p /= (-k);
+    }
+    cout << cnt << nl;
+    forn(i, cnt) cout << nums[i] << " ";
+}
