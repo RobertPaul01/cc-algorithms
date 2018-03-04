0c119802487a9aa5fb1ec34651d0793364b013d0
Robert <robert20paul@gmail.com>
2018-03-03 20:27:32 -0600

    finished TaskD

--- codeforces/464d2/taskD.cc
+++ codeforces/464d2/taskD.cc
@@ -11,6 +11,45 @@ using namespace std;
 
 typedef long long ll;
 
+const int eng = 'z' + 1;
+char dsu[eng][2];
+
+char find(char c) {
+    if (dsu[c][0] != c) {
+        dsu[c][0] = find(dsu[c][0]);
+    }
+    return dsu[c][0];
+}
+
 signed main() { _
+    int n; cin >> n;
+    string a, b; cin >> a >> b;
+    forn(i, eng) {
+        dsu[i][0] = i;
+        dsu[i][1] = 0;
+    }
     
+    int count = 0;
+    forn(i, n) {
+        char aRoot = find(a[i]);
+        char bRoot = find(b[i]);
+        if (aRoot != bRoot) {
+            count ++;
+            if (dsu[aRoot][1] < dsu[bRoot][1]) {
+                dsu[aRoot][0] = bRoot;
+            } else if (dsu[aRoot][1] > dsu[bRoot][1]) {
+                dsu[bRoot][0] = aRoot;
+            } else {
+                dsu[aRoot][0] = bRoot;
+                dsu[aRoot][1]++;
+            }
+        }
+    }
+    
+    cout << count << endl;
+    forn(i, eng) {
+        if (dsu[i][0] != i) {
+            cout << (char)i << " " << dsu[i][0] << nl;
+        }
+    }
 }
