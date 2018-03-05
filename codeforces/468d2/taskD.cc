06aa2a196ee58aef34d146895faf8cbc014b245a
Robert <robert20paul@gmail.com>
2018-03-04 21:58:44 -0600

    updates

--- codeforces/468d2/taskD.cc
+++ codeforces/468d2/taskD.cc
@@ -12,5 +12,29 @@ using namespace std;
 typedef long long ll;
 
 signed main() { _
+    int n; cin >> n;
+
+    vector<int> out[n + 7];
+    forn(i, n - 1) {
+        int ai; cin >> ai;
+        out[ai - 1].push_back(i + 1);
+    }
+
+    int rounds[n + 7];
+    memset(rounds, 0, sizeof(rounds));
     
+    queue<pii> s;
+    s.push({0, 0});
+    while (!s.empty()) {
+        auto top = s.front(); s.pop();
+        rounds[top.second]++;
+        for (int x : out[top.first]) {
+            s.push({x, top.second + 1});
+        }
+    }
+    
+    int count = 0;
+    for (int x : rounds) count += (x % 2);
+    
+    cout << count << endl;
 }
